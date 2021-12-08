a2504b4745672a8339e079b5c3fa17f70eee8d0a
harenje <67513775+harenje@users.noreply.github.com>
2021-12-08 08:59:00 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $link=mysqli_connect("ec2-54-155-87-214.eu-west-1.compute.amazonaws.com","98c5383015870b5e07b13500ae80b35b46f33c52b796b0be0180e1765dcf3344");
+    $link=mysqli_connect("ec2-54-155-87-214.eu-west-1.compute.amazonaws.com","hwbejbwtpipnay","98c5383015870b5e07b13500ae80b35b46f33c52b796b0be0180e1765dcf3344");
     if(mysqli_connect_error()){
         die("Database connect error");
     }
