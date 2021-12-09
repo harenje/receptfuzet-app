b5765cfb88f96b07aff7732ab8128aa0151c7d96
harenje <67513775+harenje@users.noreply.github.com>
2021-12-09 21:24:38 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $link=mysqli_connect("eu-cdbr-west-01.cleardb.com","b0b85efb45e489","01f8b4ef");
+    $link=mysqli_connect("","","");
     if(mysqli_connect_error()){
         die("Database connect error");
     }
